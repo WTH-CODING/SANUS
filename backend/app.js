dc1b4ecf13c5f4c2263fef99a65bc4c5a3f3dede
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-02-17 20:51:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const path = require("path");
 const express = require("express");
 const app = express();
+const httpapp = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -27,6 +28,10 @@ mongoose
     console.log("DB CONNECTED");
   });
 
+httpapp.get("*", function (req, res, next) {
+  res.redirect("https://" + req.headers.host + req.path);
+});
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
@@ -48,7 +53,7 @@ app.get('/', function (req, res) {
   res.send(__dirname + 'index.html');
   });
 
-const httpServer = http.createServer(app);
+const httpServer = http.createServer(httpapp);
 httpServer.listen(80, () => {
   console.log("Server running on port 80");
 });
