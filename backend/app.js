0b957ec700a37f444fb9eed98500f5f362ca85e6
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-02-17 20:46:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const http = require("http");
+const https = require("https");
 
 //My routes
 const authRoutes = require("./routes/auth");
@@ -25,7 +26,6 @@ mongoose
     console.log("DB CONNECTED");
   });
 
-
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
@@ -35,15 +35,28 @@ app.use("/api", userRoutes);
 app.use("/api", healthIssueRoutes);
 app.use("/api", reportRoutes);
 
-app.use("/check", (req,res)=>{
-    res.send("Hanji serve chal raha sojao!");
+app.use("/check", (req, res) => {
+  res.send("Hanji serve chal raha sojao!");
 });
-app.use(express.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-  res.send(__dirname + 'index.html');
-  });
+app.use(express.static(__dirname + "/public"));
+app.get("/", function (req, res) {
+  res.send(__dirname + "index.html");
+});
 
 const httpServer = http.createServer(app);
 httpServer.listen(80, () => {
   console.log("Server running on port 80");
 });
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("/etc/letsencrypt/live/jaagrat.ml/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/jaagrat.ml/cert.pem"),
+      ca: fs.readFileSync("/etc/letsencrypt/live/jaagrat.ml/fullchain.pem"),
+    },
+    app
+  )
+  .listen(443, () => {
+    console.log("Listening...");
+  });
